787dc8f545f107231614a6a403106f8f29ef654a
archandla <d.a.n.202021@gmail.com>
2024-05-14 14:19:02 +0300

    ost_segment with syn,rst flags

--- core/ost_segment.c
+++ core/ost_segment.c
@@ -2,20 +2,83 @@
 
 #include <string.h>
 
-bool is_ack(const OstSegmentHeader* const header){
-    return header->flag & 0x80;
+int8_t peekHeader(const SpWPacket *const packet, OstSegmentHeader *const header)
+{
+    if (packet->sz < sizeof(OstSegmentHeader))
+        return -1;
+    if (memcpy(header, packet->data, sizeof(OstSegmentHeader)) != sizeof(OstSegmentHeader))
+        return -1;
+    return 0;
 }
 
-int8_t peekHeader(const SpWPacket* const packet, OstSegmentHeader* const header) {
-    if(packet->sz < sizeof(OstSegmentHeader)) return -1;
-    if(memcpy(header, packet->data, sizeof(OstSegmentHeader)) != sizeof(OstSegmentHeader)) return -1;
-    return 0;    
-}
-
-void set_type(OstSegmentHeader* const header, const SegmentType t) {
-
-}
-
-int8_t data_to_ost_segment(OstSegment* const seg, void* data, uint8_t sz){
+int8_t data_to_ost_segment(OstSegment *const seg, void *data, uint8_t sz)
+{
     return 1;
+}
+void set_seq_number(OstSegment *const seg, uint8_t n)
+{
+    seg->header.seq_number = n;
+}
+uint8_t get_seq_number(const OstSegment *const seg)
+{
+    return seg->header.seq_number;
+}
+
+void set_src_addr(OstSegment *const seg, uint8_t addr)
+{
+    seg->header.source_addr = addr;
+}
+
+uint8_t get_src_addr(const OstSegment *const seg)
+{
+    return seg->header.source_addr;
+}
+
+void set_payload_len(OstSegment *const seg, uint8_t l)
+{
+    seg->header.payload_length = l;
+}
+
+uint8_t get_payload_len(const OstSegment *const seg)
+{
+    return seg->header.payload_length;
+}
+
+void set_flag(OstSegment *const seg, SegmentFlag flag)
+{
+    switch (flag)
+    {
+    case ACK:
+        seg->header.flags |= 0b00000001;
+        break;
+    case SYN:
+        seg->header.flags |= 0b00000010;
+        break;
+    case RST:
+        seg->header.flags |= 0b00000100;
+        break;
+    default:
+        seg->header.flags &= 0b11111000;
+        break;
+    }
+}
+
+int8_t is_ack(const OstSegment *const seg)
+{
+    return seg->header.flags & 0b00000001;
+}
+
+int8_t is_syn(const OstSegment *const seg)
+{
+    return seg->header.flags & 0b00000010;
+}
+
+int8_t is_rst(const OstSegment *const seg)
+{
+    return seg->header.flags & 0b00000100;
+}
+
+int8_t is_dta(const OstSegment *const seg)
+{
+    return seg->header.flags & 0b00000000 == 0;
 }
--- core/ost_segment.h
+++ core/ost_segment.h
@@ -1,33 +1,56 @@
+#ifndef OST_SEGMENT_H
+#define OST_SEGMENT_H
+
 #include <inttypes.h>
 #include <stdbool.h>
 
 #include "spw_packet.h"
 
-#ifndef OST_SEGMENT_H
-#define OST_SEGMENT_H
-
 typedef uint8_t FlagOctet;
 
-typedef enum {
-    DATA = 0,
-    ACK = 1
-} SegmentType;
+typedef enum
+{
+    ACK,
+    SYN,
+    RST,
+    DTA // virtual
+} SegmentFlag;
 
-typedef struct {
-    FlagOctet flag;
+typedef struct
+{
+    FlagOctet flags;
     uint8_t seq_number;
     uint8_t source_addr;
-    uint8_t payload_length;
+    uint16_t payload_length;
 } OstSegmentHeader;
 
-typedef struct { 
+typedef struct {
     OstSegmentHeader header;
-    uint8_t* payload;
+    uint8_t *payload;
+    uint32_t payload_size;
 } OstSegment;
 
-int8_t data_to_ost_segment(OstSegment* const seg, void* data, uint8_t sz);
-int8_t peekHeader(const SpWPacket* const packet, OstSegmentHeader* const header);
-void set_type(OstSegmentHeader* const header, const SegmentType t);
-bool is_ack(const OstSegmentHeader* const header);
+int8_t data_to_ost_segment(OstSegment *const seg, void *data, uint8_t sz);
+int8_t spw_packet_to_ost_segment(const SpWPacket *const packet, OstSegment *const seg);
+int8_t peekHeader(const SpWPacket *const packet, OstSegmentHeader *const header);
+
+void set_seq_number(OstSegment *const header, uint8_t);
+uint8_t get_seq_number(const OstSegment *const header);
+
+void set_src_addr(OstSegment *const header, uint8_t);
+uint8_t get_src_addr(const OstSegment *const header);
+
+void set_payload_len(OstSegment *const header, uint8_t);
+uint8_t get_payload_len(const OstSegment *const header);
+
+void set_flag(OstSegment *const header, SegmentFlag flag);
+void unset_flag(OstSegment *const header, SegmentFlag flag);
+
+int8_t is_ack(const OstSegment* const header);
+int8_t is_syn(const OstSegment* const header);
+int8_t is_rst(const OstSegment* const header);
+int8_t is_dta(const OstSegment* const header);
+
+segment_type_name(SegmentFlag t);
 
 #endif
