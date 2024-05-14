5eb37ffcf72013988a67d2b5a2403894b64309d5
archandla <d.a.n.202021@gmail.com>
2024-05-14 15:16:43 +0300

    start ost_node mc30sf6 implementation

--- core/ost_node.c
+++ core/ost_node.c
@@ -1,98 +1,151 @@
 #include "ost_node.h"
 
-void send_to_application(const OstSegment* const node);
-int8_t get_packet_from_application();
-int8_t spw_layer_send(void* spw_layer, const OstSegment* const seg);  
+// #define LOOPBACK_MODE
+#define TRY 100
+#define SWIC_SPEED 100
 
-int8_t get_packet_from_physical() {
-    return 1;
+void send_to_application(const OstNode* const node, OstSegment* segment);
+int8_t get_packet_from_application(OstNode* const node);
+void add_packet_to_rx(OstNode* const node, OstSegment* segment);
+
+int8_t send_to_physical(const OstNode* const node, SegmentFlag t, uint8_t seq_n);
+int8_t get_packet_from_physical(const OstNode* const node);
+int8_t tx_sliding_window_have_space(const OstNode* const node);
+int8_t in_tx_window(const OstNode* const node, uint8_t);
+int8_t in_rx_window(const OstNode* const node, uint8_t);
+int8_t hw_timer_handler(uint8_t);
+
+int8_t network_layer_handler(OstNode* const node, const SpWPacket* const pkt);
+
+int8_t aggregate_socket(uint8_t address);
+int8_t delete_socket(uint8_t address);
+
+void event_name(TransportLayerEvent e);
+
+int8_t start(OstNode *const node, int8_t socket_mode) {
+    return spw_hw_init(node);
 }
 
-
-bool tx_sliding_window_have_space();
-bool in_tx_window(const OstNode* const node, uint8_t);
-bool in_rx_window(const OstNode* const node, uint8_t);
-bool hw_timer_handler(OstNode* const node, uint8_t);
-
-int8_t spw_layer_send(void* spw_layer, const OstSegment* const seg) {
-    return 1;
-}
-
-void send_to_application(const OstSegment* const s) {
-    // some output
-}
-
-int8_t add_packet_to_tx(OstNode* const node, void* data, uint8_t sz) {
-    if(tx_sliding_window_have_space(node) && sz <= UINT8_MAX) {
-        node->acknowledged[node->tx_window_top] = false;
-        data_to_ost_segment(&node->tx_buffer[node->tx_window_top], data, sz);
-        node->tx_window_top = (node->tx_window_top + 1)  % MAX_UNACK_PACKETS;
-        return 0;
-    }
-    return 1;
-}
-
-bool tx_sliding_window_have_space(const OstNode* const node) {
-    if(node->tx_window_top >= node->tx_window_bottom) {
-        return node->tx_window_top - node->tx_window_bottom < WINDOW_SZ;
-    } else {
-        return MAX_UNACK_PACKETS - node->tx_window_top + 1 + node->tx_window_bottom < WINDOW_SZ;
+void shutdown(OstNode *const node) {
+    // clear timer table
+    uint8_t i = 0;
+    for(i = 0; i < PORTS_NUMBER; ++i) {
+        if(node->ports[i]->to_address != node->self_address && node->ports[i]->state != CLOSED) {
+            close(node->ports[i]);
+        }
     }
 }
 
-bool in_tx_window(const OstNode* const node, uint8_t seq_n) {
-    return (node->tx_window_top >= node->tx_window_bottom && seq_n >= node->tx_window_bottom && seq_n < node->tx_window_top) ||
-           (node->tx_window_bottom > node->tx_window_top && (seq_n >= node->tx_window_bottom || seq_n < node->tx_window_top));
+int8_t event_handler(OstNode *const node, const TransportLayerEvent e) {
+
 }
 
-bool in_rx_window(const OstNode* const node, uint8_t seq_n) {
-    return (node->rx_window_top >= node->rx_window_bottom && seq_n >= node->rx_window_bottom && seq_n < node->rx_window_top) ||
-           (node->rx_window_bottom > node->rx_window_top && (seq_n >= node->rx_window_bottom || seq_n < node->rx_window_top));
-}
+int8_t send_packet(OstNode *const node, int8_t address, const uint8_t *buffer, uint32_t size);
+int8_t receive_packet(OstNode *const node, const uint8_t *buffer, uint32_t *received_sz);
 
-int8_t send_to_physical(OstNode* const node, SegmentType t, uint8_t seq_n) {
-    if(t == DATA) {
-        node->tx_buffer[seq_n].header.seq_number = seq_n;
-        set_type(&node->tx_buffer[seq_n].header, DATA);
-        node->tx_buffer[seq_n].header.source_addr = -1;
+int8_t open_connection(OstNode *const node, uint8_t address, int8_t mode);
+int8_t close_connection(OstNode *const node, uint8_t address);
+int8_t get_socket(OstNode *const node, uint8_t address, OstSocket *const socket);
 
-        spw_layer_send(node->spw_layer, &node->tx_buffer[seq_n]);
-        add_new_timer(&node->queue, node->tx_window_bottom, 100);
-    } else if (t == ACK) {
-        OstSegment seg = { 
-            .header = {
-                .payload_length = 0,
-                .seq_number = seq_n,
-                .source_addr = -1
-            }
-        };
-        set_type(&seg.header, ACK);
-        spw_layer_send(node->spw_layer, &seg);
-    }
-    return 0;
-}
 
-bool hw_timer_handler(OstNode* const node, uint8_t seq_n) {
-    node->to_retr = seq_n;
-    event_handler(node, RETRANSMISSION_INTERRUPT);
-    return true;
-}
+// void swic_test(void) {
+//   unsigned char src[SIZE] __attribute__((aligned(8))) = {
+//       0,
+//   };
+//   unsigned char dst[SIZE] __attribute__((aligned(8))) = {
+//       0,
+//   };
+//   unsigned int desc[2] __attribute__((aligned(8))) = {
+//       0,
+//   };
+//   unsigned int i = 0;
 
-int8_t event_handler(OstNode* const node, const TransportLayerEvent e) {
-    switch (e)
-    {
-        case PACKET_ARRIVED_FROM_NETWORK:
-            get_packet_from_physical();
-            break;
-        case APPLICATION_PACKET_READY:
-            send_to_physical(node, DATA, (node->tx_window_top + MAX_UNACK_PACKETS - 1) % MAX_UNACK_PACKETS);
-            break;
-        case RETRANSMISSION_INTERRUPT:
-            send_to_physical(node, DATA, node->to_retr);
-            break;
-        default:
-            break;
-    }
+// #ifdef LOOPBACK_MODE
+//   unsigned int flag0 = 0;
+//   unsigned int flag1 = 0;
+// #endif
 
-    return 0;
+//   base_init();
+
+// #ifdef LOOPBACK_MODE
+//   // SWIC 1
+//   for (i = 0; i <= TRY; i++) {
+//     swic_init_loopback(1);
+
+//     if (swic_is_connected(1) == 1)
+//       break;
+//     debug_printf("SWIC 1 MODE: %x \n", GIGASPWR_COMM_SPW_MODE(1));
+//     debug_printf("SWIC 1 STATUS: %x \n", GIGASPWR_COMM_SPW_STATUS(1));
+//     delay(1000);
+//   }
+//   flag1 = swic_is_connected(1);
+//   debug_printf("SWIC 1 CONNECT: %x \n", flag1);
+
+//   // SWIC0
+//   i = 0;
+//   for (i = 0; i <= TRY; i++) {
+//     swic_init_loopback(0);
+
+//     if (swic_is_connected(0) == 1)
+//       break;
+//     debug_printf("SWIC 0 MODE: %x \n", GIGASPWR_COMM_SPW_MODE(0));
+//     debug_printf("SWIC 0 STATUS: %x \n", GIGASPWR_COMM_SPW_STATUS(0));
+//     delay(1000);
+//   }
+//   flag0 = swic_is_connected(0);
+//   debug_printf("SWIC 0 CONNECT: %x \n", flag0);
+
+//   i = 0;
+//   if ((flag0 == 0) & (flag1 == 0)) {
+//     debug_printf("ALL SWIC CONNECTION FAIL! \n");
+//     return;
+//   }
+
+// #endif
+
+int8_t spw_hw_init(OstNode* const node) {
+#ifndef LOOPBACK_MODE
+  // CABLE SWIC0 <-> SWIC1
+  uint32_t i;
+  for (i = 0; i <= TRY; i++) {
+    swic_init(1);
+    swic_init(0);
+    delay(1000);
+    if (swic_is_connected(1) == 1)
+      break;
+    debug_printf("SWIC 1 MODE: %x \n", GIGASPWR_COMM_SPW_MODE(1));
+    debug_printf("SWIC 0 MODE: %x \n", GIGASPWR_COMM_SPW_MODE(0));
+    debug_printf("SWIC 1 STATUS: %x \n", GIGASPWR_COMM_SPW_STATUS(1));
+    debug_printf("SWIC 0 STATUS: %x \n", GIGASPWR_COMM_SPW_STATUS(0));
+  }
+
+  if (swic_is_connected(1) == 0) {
+    debug_printf("OST SWIC CONNECTION FAIL! \n");
+    return -1;
+  }
+
+  debug_printf("TRY SPEED_SET: %u \n", SWIC_SPEED);
+
+  for (i = 0; i <= (TRY - 1); i++) {
+    swic_set_tx_speed(0, SWIC_SPEED);
+    swic_set_tx_speed(1, SWIC_SPEED);
+
+    if (swic_is_connected(1) == 1)
+      break;
+
+    swic_init(1);
+    swic_init(0);
+    delay(1000);
+    swic_set_tx_speed(0, SWIC_SPEED);
+    swic_set_tx_speed(1, SWIC_SPEED);
+  }
+
+  if (swic_is_connected(1) == 0) {
+    debug_printf("OST hardware Init FAIL! \n");
+    return;
+  }
+
+  debug_printf("OST 0 RX_SPEED: %x \n", swic_get_rx_speed(0));
+  debug_printf("OST 1 RX_SPEED: %x \n", swic_get_rx_speed(1));
+#endif
 }
--- core/ost_node.h
+++ core/ost_node.h
@@ -6,8 +6,13 @@
 #include "timer_fifo.h"
 #include "ost_segment.h"
 #include "spw_packet.h"
+#include "ost_socket.h"
 
-#define WINDOW_SZ 1
+// #define WINDOW_SZ 1
+
+static const uint16_t WINDOW_SZ = 10;
+static const micros_t DURATION_RETRANSMISSON = 500000;
+static const uint8_t PORTS_NUMBER = 3;
 
 /**
  * \defgroup ost Open SpaceWire Transport Layer Node
@@ -16,27 +21,39 @@
 typedef enum {
     PACKET_ARRIVED_FROM_NETWORK = 0,
     APPLICATION_PACKET_READY,
-    CHECKSUM_ERR,
     RETRANSMISSION_INTERRUPT,
     TRANSPORT_CLK_INTERRUPT,
 } TransportLayerEvent;
 
 typedef struct {
-    uint8_t tx_window_top;
-    uint8_t tx_window_bottom;
-    uint8_t rx_window_top;
-    uint8_t rx_window_bottom;
-    uint8_t to_retr;
+    TimerFifo queue;
 
+    uint8_t to_retr;
+    uint8_t tx_window_bottom;
+    uint8_t tx_window_top;
+    uint8_t rx_window_bottom;
+    uint8_t rx_window_top;
     OstSegment tx_buffer[MAX_UNACK_PACKETS];
     OstSegment rx_buffer[MAX_UNACK_PACKETS];
     int8_t acknowledged[MAX_UNACK_PACKETS];
-
     TimerFifo queue;
-    void* spw_layer;
+    void *spw_layer;
+
+    uint8_t self_address;
+    void (*rx_cb)(uint8_t, OstSegment *);
+    OstSocket *ports[MAX_UNACK_PACKETS];
 } OstNode;
 
-int8_t event_handler(OstNode * const node, const TransportLayerEvent e);
-int8_t add_packet_to_tx(OstNode* const node, void* data, uint8_t sz);
+int8_t start(OstNode *const node, int8_t socket_mode);
+void shutdown(OstNode *const node);
+int8_t event_handler(OstNode *const node, const TransportLayerEvent e);
+
+int8_t open_connection(OstNode *const node, uint8_t address, int8_t mode);
+int8_t close_connection(OstNode *const node, uint8_t address);
+int8_t send_packet(OstNode *const node, int8_t address, const uint8_t *buffer, uint32_t size);
+int8_t receive_packet(OstNode *const node, const uint8_t *buffer, uint32_t *received_sz);
+int8_t get_socket(OstNode *const node, uint8_t address, OstSocket *const socket);
+
+// Ptr<SpWDevice> GetSpWLayer();
 
 #endif
